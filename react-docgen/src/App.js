62c975238aadaa584d6fc490d3025e78e72b54bc
MrFirthy <ashley@mrfirthy.me>
2017-09-21 19:03:28 +0100

    Remove react content from home page

--- react-docgen/src/App.js
+++ react-docgen/src/App.js
@@ -1,23 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import data from './components.json';
 import Docs from './Docs';
 import './App.css';
-
-console.log(data);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
         <Docs data={data} />
       </div>
     );
