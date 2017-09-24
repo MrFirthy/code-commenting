1a3e80231d9ebbd4acfbfc1365db20fab23cf11a
MrFirthy <ashley@mrfirthy.me>
2017-09-24 17:40:48 +0100

    Style react-docgen Docs page

--- react-docgen/src/App.js
+++ react-docgen/src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import data from './docs/components.json';
-import Docs from './Docs';
-import Square from './components/Square';
+import Docs from './components/Docs';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Square text="Wubalubadubdub" />
         <Docs data={data} />
       </div>
     );
--- react-docgen/src/components/Square.js
+++ react-docgen/src/components/Square.js
@@ -7,8 +7,9 @@ import PropTypes from 'prop-types';
 class Square extends React.Component {
 
     render() {
+        const squareClass = this.props.isSpinning ? 'Square spinning' : 'Square';
         return (
-            <div>
+            <div className={squareClass} onClick={this.props.handleClick}>
                 <h2>Square</h2>
                 <p>{this.props.text}</p>
             </div>
@@ -20,7 +21,19 @@ Square.propTypes =  {
 	/**
 	 * The text for the square
 	 */
-    text: PropTypes.string.isRequired
+    text: PropTypes.string.isRequired,
+    /**
+     * Determines whether the square is spinning or not
+     */
+    isSpinning: PropTypes.bool.isRequired,
+    /**
+     * The function that is triggered when the square is clicked
+     */
+    handleClick: PropTypes.func,
+    /**
+     * An optional object of styles for the square
+     */
+    styles: PropTypes.object
 }
 
 export default Square
